9658d163878862c51347b4e6da8c290df22591f4
Calvin <cookiecaow@gmail.com>
2014-05-17 20:08:48 +1000

    Login implemented

--- app/src/main/java/com/id11413010/circle/app/Constants.java
+++ app/src/main/java/com/id11413010/circle/app/Constants.java
@@ -1,14 +1,5 @@
 package com.id11413010.circle.app;
-
-/**
- * Created by Calvin on 11/05/2014.
- */
 public class Constants {
-    public static final String FRIENDS = "friend";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_FIRSTNAME = "firstName";
-    public static final String COLUMN_LASTNAME = "lastName";
-    public static final String COLUMN_PICTURE = "picture";
-    public static final int DB_VERSION = 1;
-
+    public static final String PREFERENCES = "MyPreferences";
+    public static final String USERID = "UserID";
 }
--- app/src/main/java/com/id11413010/circle/app/friends/Friend_List.java
+++ app/src/main/java/com/id11413010/circle/app/friends/Friend_List.java
@@ -1,47 +1,8 @@
 package com.id11413010.circle.app.friends;
 
 import android.app.ListActivity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.SimpleCursorAdapter;
-
-import com.id11413010.circle.app.R;
 
 
 public class Friend_List extends ListActivity {
 
-    private FriendDatabaseHelper db;
-    private SimpleCursorAdapter adapter;
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        db = new FriendDatabaseHelper(this);
-        adapter = new SimpleCursorAdapter(this, R.layout.activity_friend, db.getUsers(), db.from(), db.to());
-        setListAdapter(adapter);
-    }
-
-    private void initialise() {
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.friend, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.addFriendMenu) {
-            startActivity(new Intent(this, Friend_Add.class));
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
