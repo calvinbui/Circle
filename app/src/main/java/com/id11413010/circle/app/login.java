fdaefebc0eae0ffbe7d49b1dfe8d13fe8c0cbccf
Calvin <cookiecaow@gmail.com>
2014-05-19 23:30:44 +1000

    This is what I want

--- app/src/main/java/com/id11413010/circle/app/login.java
+++ app/src/main/java/com/id11413010/circle/app/login.java
@@ -1,17 +1,45 @@
+
 package com.id11413010.circle.app;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class login extends Activity {
+public class Login extends Activity {
+
+    private EditText username_et;
+    private EditText password_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username_et = (EditText)findViewById(R.id.username_et);
+        password_et = (EditText)findViewById(R.id.password_et);
     }
 
 
@@ -28,9 +56,66 @@ public class login extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    public void loginUser(View v) {
+        new LoginTask().execute();
+    }
+
+    public void openRegisterPage(View v) {
+        startActivity(new Intent(this, Register.class));
+    }
+
+    private class LoginTask extends AsyncTask<Void, Void, Void> {
+
+        String username;
+        String password;
+        String htmlResponse;
+
+        protected void onPreExecute() {
+            username = username_et.getText().toString();
+            password = password_et.getText().toString();
         }
-        return super.onOptionsItemSelected(item);
+
+        protected Void doInBackground(Void... params) {
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://calvinbui.no-ip.biz/android/login.php?");
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("username", username));
+            nameValuePairs.add(new BasicNameValuePair("password", password));
+
+            try {
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                htmlResponse = EntityUtils.toString(entity);
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            String[] parts = htmlResponse.split(":");
+            if (parts[0].equals("loginCorrect")) {
+                SharedPreferences sp = getSharedPreferences(Constants.PREFERENCES, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString(Constants.USERID, parts[1]);
+                editor.putString(Constants.FIRSTNAME, parts[2]);
+                editor.putString(Constants.LASTNAME, parts[3]);
+                editor.putString(Constants.CIRCLE, parts[4]);
+                editor.commit();
+                startActivity(new Intent(Login.this, HomeScreen.class));
+            }
+            else {
+                Toast.makeText(getApplicationContext(),R.string.loginError, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
