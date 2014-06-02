740a6e4e9695862bf99b645e053444459c1427cc
calvinbui <cookiecaow@gmail.com>
2014-06-03 09:19:50 +1000

    Capitals allowed in name

--- app/src/main/java/com/id11413010/circle/app/network/Network.java
+++ app/src/main/java/com/id11413010/circle/app/network/Network.java
@@ -25,8 +25,7 @@ public class Network {
     /**
      * The URL for the web service.
      */
-    private static String Url = "http://calvinbui.no-ip.biz/android/";
-    //private static String Url = "http://192.168.42.193/android/";
+    private static String Url = "http://192.168.43.63/android/";
 
     /**
      * Connection to the database through a HTTP Client.
